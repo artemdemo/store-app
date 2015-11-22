0cdb472bcfd358974fc65c1cbbad61500161cbce
Artem Demo <artemdemo@gmail.com>
2015-11-22 22:18:42 +0300

    [ReactJS+TS] added browserify

--- ReactJS+TypeScript/Gulpfile.js
+++ ReactJS+TypeScript/Gulpfile.js
@@ -1,15 +1,25 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var less = require('gulp-less');
+var browserify = require('gulp-browserify');
+var shell = require('gulp-shell');
 
 gulp.task('ts', function() {
-    gulp.src('source/**/*.tsx')
+    return gulp.src('source/**/*.tsx')
         .pipe(ts({
             target: 'es5',
             module: 'commonjs',
             jsx: 'react'
         }))
         .js.pipe(gulp.dest('js/tmp'));
+});
+
+gulp.task('browserify', function() {
+    return gulp.src('js/tmp/app.js')
+        .pipe(browserify({
+
+        }))
+        .pipe(gulp.dest('js'));
 });
 
 gulp.task('less', function () {
@@ -19,10 +29,10 @@ gulp.task('less', function () {
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./source/**/*.ts', ['ts']);
+    gulp.watch('./source/**/*.ts', ['ts', 'browserify']);
     gulp.watch('./source/less/*.less', ['less']);
 });
 
-gulp.task('build', ['ts', 'less']);
+gulp.task('build', ['ts', 'browserify', 'less']);
 
 gulp.task('default', ['build', 'watch']);

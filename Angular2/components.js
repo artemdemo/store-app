4a84297b0605db613be762fefd1179a30f14a593
Artem Demo <artemdemo@gmail.com>
2015-11-23 08:29:49 +0300

    [ReactJS+TS] updated readme.md

--- Angular2/components.js
+++
@@ -1,6 +0,0 @@
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('components/Home'));
-__export(require('components/Store'));
-//# sourceMappingURL=components.js.map
--- Angular2/typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="rx/rx-lite.d.ts" />
-/// <reference path="rx/rx.d.ts" />
-/// <reference path="angular2/angular2.d.ts" />
-/// <reference path="angular2/router.d.ts" />
